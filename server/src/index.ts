6509c6d1f3a76f1cfb8fce4468921370f5ef24f0
unloopedmido <youarenotmido@gmail.com>
2025-04-05 03:26:06 +0300

    fix: cors issue

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,11 +1,16 @@
 import Express from "express";
 import Chalk from "chalk";
+import Cors from 'cors';
 
 const Port = process.env.SERVER_PORT || 3001;
 const App = Express();
 
+App.use(Cors({
+  origin: ["http://localhost:5173"],
+  credentials: true
+}))
 App.get("/", (req, res) => {
-    res.send("Hello, World!");
+  res.send("Hello, World!");
 })
 
 import AuctionRoute from "./routes/auctions";
@@ -15,5 +20,5 @@ import UsersRoute from "./routes/users";
 App.use("/users", UsersRoute);
 
 App.listen(Port, () => {
-	console.log(Chalk.green(`[SERVER]`), `Server running on port ${Port}`);
+  console.log(Chalk.green(`[SERVER]`), `Server running on port ${Port}`);
 });
